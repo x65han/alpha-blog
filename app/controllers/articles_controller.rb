31835c1413b753ba65432cf0ec10ddffb67d7057
Johnson Han <Johnson.Han@sony.com>
2017-06-22 14:18:07 -0400

    before_action for controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:destroy, :show, :edit, :update]
     def index
         @articles = Article.all
     end
@@ -8,7 +9,6 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
 
         redirect_to articles_path
@@ -27,15 +27,14 @@ class ArticlesController < ApplicationController
     end
 
     def show 
-        @article = Article.find(params[:id])
+
     end
 
     def edit
-        @article = Article.find(params[:id])
+
     end
 
     def update
-        @article = Article.find(params[:id])
         if @article.update(article_params)   
             flash[:notice] = "Article was successfully updated"
             redirect_to article_path(@article)
@@ -43,7 +42,12 @@ class ArticlesController < ApplicationController
             render 'edit'
         end
     end
+
     private 
+    def set_article
+        @article = Article.find(params[:id]) 
+    end
+
     def article_params
         params.require(:article).permit(:title, :description)
     end
