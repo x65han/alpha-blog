553d61ad822cbb0517a3725ae635c19dcc5493dc
x65han <x65han@uwaterloo.ca>
2017-06-23 21:25:26 -0400

    styling on show.html.erb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,14 +12,14 @@ class ArticlesController < ApplicationController
         @article.destroy
 
         redirect_to articles_path
-        flash[:notice] = "Article was successfully deleted"
+        flash[:danger] = "Article was successfully deleted"
     end
 
     def create
         @article = Article.new(article_params)
 
         if @article.save   
-            flash[:notice] = "Article was successfully created"
+            flash[:success] = "Article was successfully created"
             redirect_to article_path(@article)
         else 
             render 'new'
@@ -36,7 +36,7 @@ class ArticlesController < ApplicationController
 
     def update
         if @article.update(article_params)   
-            flash[:notice] = "Article was successfully updated"
+            flash[:success] = "Article was successfully updated"
             redirect_to article_path(@article)
         else 
             render 'edit'
