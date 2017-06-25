97c11756c1d406b628f25720600d19ea658c574e
x65han <x65han@uwaterloo.ca>
2017-06-25 11:23:26 -0400

    sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "pages#home", as: "home"
-  get '/about', to: 'pages#about'
-
+  get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
   resources :articles
 end
