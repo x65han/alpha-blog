1bc38f5b504d36fc8f7903a5f2c1033abad2694b
x65han <x65han@uwaterloo.ca>
2017-06-25 18:37:57 -0400

    update show article html erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
-    before_action :set_user, only: [:edit, :update]
+    before_action :set_user, only: [:edit, :update, :show]
 
+    def index
+        @users = User.all
+    end
     def new
         @user = User.new
     end
@@ -28,6 +31,9 @@ class UsersController < ApplicationController
         end
     end
 
+    def show
+        
+    end
     private 
     def set_user
         @user = User.find(params[:id]) 
