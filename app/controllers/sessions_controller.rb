2188de5d71274e9b0c9279d42482654b898150d3
x65han <x65han@uwaterloo.ca>
2017-06-26 18:13:39 -0400

    login & logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,19 @@
 class SessionsController < ApplicationController
+    def create
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash[:success] = "You have successfully logged in"
+            redirect_to user_path(user)
+        else
+            flash.now[:danger] = "Something is wrong with your login information"
+            render 'new'
+        end
+    end
+
+    def destroy
+        session[:user_id] = nil
+        flash[:success] = "You have successfully logged out"
+        redirect_to home_path
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root "pages#home", as: "home"
   get 'about', to: 'pages#about'
   get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   post 'users', to: 'users#create'
   resources :users, except: [:new]
   resources :articles
